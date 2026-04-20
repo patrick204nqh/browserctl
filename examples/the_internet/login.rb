246688e5c586d2028b90c178994d0eb7f1e9c626
Patrick <patrick204nqh@gmail.com>
2026-04-21 00:45:34 +0700

    fix: save login screenshot to ~/.browserctl/screenshots/ instead of docs/ path

--- examples/the_internet/login.rb
+++ examples/the_internet/login.rb
@@ -3,9 +3,10 @@
 Browserctl.workflow "the_internet/login" do
   desc "Form authentication: fill credentials, submit, assert secure area"
 
-  param :username, default: "tomsmith"
-  param :password, default: "SuperSecretPassword!", secret: true
-  param :base_url, default: "https://the-internet.herokuapp.com"
+  param :username,        default: "tomsmith"
+  param :password,        default: "SuperSecretPassword!", secret: true
+  param :base_url,        default: "https://the-internet.herokuapp.com"
+  param :screenshot_path, default: File.expand_path("~/.browserctl/screenshots/the_internet_login.png")
 
   step "open login page" do
     client.open_page("main", url: "#{base_url}/login")
@@ -21,8 +22,7 @@ Browserctl.workflow "the_internet/login" do
     assert page(:main).url.include?("/secure"), "expected redirect to /secure"
     flash = client.evaluate("main", "document.querySelector('.flash.success')?.innerText?.trim()")[:result]
     assert flash&.include?("You logged into a secure area!"), "expected success flash, got: #{flash.inspect}"
-    screenshots_dir = File.expand_path("../../docs/screenshots", __dir__)
-    page(:main).screenshot(path: "#{screenshots_dir}/the_internet_login.png")
+    page(:main).screenshot(path: screenshot_path)
   end
 
   step "logout and verify" do
